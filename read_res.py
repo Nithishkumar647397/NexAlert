107b1d3be6af0aed1bcd9432d72742e06a1bf7d0
Nithishkumar <nithishkumars006@gmail.com>
2026-09-05 16:21:53 +0530

    feat: update alert processing to send data to triage API endpoint

--- read_res.py
+++ read_res.py
@@ -1,7 +1,16 @@
 import json
+import urllib.request
 
-with open('triage_result.json', encoding='utf-16le') as f:
+with open('data/alerts.json') as f:
     data = json.load(f)
+
+req = urllib.request.Request(
+    'http://localhost:8000/api/triage',
+    data=json.dumps({'alerts': data}).encode('utf-8'),
+    headers={'Content-Type': 'application/json'}
+)
+res = urllib.request.urlopen(req)
+data = json.loads(res.read().decode('utf-8'))
 
 incidents = [i for i in data['incidents'] if len(i['alerts']) > 1]
 noise = [i for i in data['incidents'] if len(i['alerts']) == 1]
